67fb69843c3f85fa7864c80c491683042a0717cb
Willer <willerrodrigo2006@hotmail.com>
2024-07-17 18:04:54 -0300

    ref: add src folder and remove components

--- App.tsx
+++ App.tsx
@@ -3,14 +3,13 @@ import './global.css';
 import 'react-native-gesture-handler';
 
 import { ApolloProvider } from '@apollo/client';
-import { client } from 'services';
 
-import RootStack from './navigation';
+import { client } from '~/services';
 
 export default function App() {
   return (
     <ApolloProvider client={client}>
-      <RootStack />
+      <></>
     </ApolloProvider>
   );
 }
