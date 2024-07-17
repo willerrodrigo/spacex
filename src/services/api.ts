acd6ed3c33f510a9d21aa3e73a25b68e5ee96a5e
Willer <willerrodrigo2006@hotmail.com>
2024-07-17 19:44:34 -0300

    ref: add .env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'https://spacex-production.up.railway.app/',
+  uri: process.env.EXPO_PUBLIC_API_URL,
   cache: new InMemoryCache(),
 });
